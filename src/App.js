631f08e999d00464cf6e4083f33ce074b16b38eb
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2024-07-29 17:01:37 +0200

    Ajout du composant Input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './componants/Header';
 import Footer from './componants/Footer';
+import Input from './componants/Input';
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
       <header className="App-header">
         <Header/>
       </header>
-
-      <Footer/>
+      <main>
+        <Input/>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+      
     </div>
   );
 }
---
+++ src/componants/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+    return(
+        <div>
+            <input type="text" className="form-control" placeholder="Nom"></input>
+        </div>
+    )
+}
+
+export default Input
